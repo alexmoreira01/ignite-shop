670e41316e989ffb965400d5ca8b086698f49aab
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-06-17 18:46:42 -0300

    Feature: Product complete

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,111 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
+import Image from 'next/image';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { stripe } from '../../lib/stripe';
+import Stripe from 'stripe';
 import { useRouter } from 'next/router';
+import axios from 'axios';
+import { useState } from 'react';
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+    defaultPriceId: string;
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data;
+
+      /* Redirecionar user -- rota externa */
+      window.location.href = checkoutUrl
+
+      /* Rota interna  
+      const router = useRouter() // acima
+
+      Router.push('/checkout')
+      */
+    } catch (err) {
+      // Conectar com uma ferramente de observalidade (Datadog / Sentry)
+
+      setIsCreatingCheckoutSession(false)
+
+      alert("Falha ao redirecionar ao checkout!")
+    }
+  }
+
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <p>Loading ...</p>
+  }
 
   return (
-    <h1>Produto: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+          Comprar Agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: 'prod_O3RFwgkFHRjjlI'} }
+    ],
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id; // Id da url;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+        defaultPriceId: price.id
+      }
+    },
+    revalidate: 60 * 60 * 1,
+  }
 }
